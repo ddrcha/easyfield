0596e16f6ce87905a43e07fed792297b3ba577e4
David Richart <contact@davidrichart.com>
2020-03-23 11:43:50 +0100

    Fix submit button name

--- src/views/materialize/submit.blade.php
+++ src/views/materialize/submit.blade.php
@@ -1,5 +1,5 @@
 <div class="input-field col s12 l{{ $width }}">
-	 <button class="btn waves-effect waves-light {{ $class }}" type="submit" name="action">{{ $name }}
+	 <button class="btn waves-effect waves-light {{ $class }}" type="submit">{{ $name }}
 		<i class="material-icons right">send</i>
 	</button>
 </div>
